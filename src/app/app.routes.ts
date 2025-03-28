dbee632f013790ed9fd1988b8fb5b4b03b0f991a
EAndresCRuiz <eder.ac.ruiz@gmail.com>
2025-03-28 15:20:47 -0500

    feat(clients): add client form component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ClientListComponent } from './features/clients/pages/client-list/client-list.component';
+import { ClientFormComponent } from './features/clients/pages/client-form/client-form.component';
 
 export const routes: Routes = [
   { path: '', component: ClientListComponent },
+  { path: 'new', component: ClientFormComponent },
   { path: '**', redirectTo: '' },
 ];
--- src/app/features/clients/pages/client-form/client-form.component.ts
+++ src/app/features/clients/pages/client-form/client-form.component.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ClientService } from 'src/app/core/services/client.service';
+import { Client } from 'src/app/core/models/client';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-client-form',
-  imports: [],
+  imports: [
+    NgIf,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './client-form.component.html',
-  styleUrl: './client-form.component.css'
+  styleUrls: ['./client-form.component.css'],
+  standalone: true,
 })
 export class ClientFormComponent {
+  clientForm: FormGroup;
+  errorMessage: string | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private clientService: ClientService,
+    private router: Router
+  ) {
+    this.clientForm = this.fb.group({
+      sharedKey: ['', [Validators.required, Validators.minLength(3)]],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.clientForm.invalid) return;
+
+    const newClient: Client = {
+      ...this.clientForm.value,
+      id: '',
+      createdAt: new Date().toISOString(),
+    };
+
+    this.clientService.createClient(newClient).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: (err) => {
+        this.errorMessage = err.error.message || 'Error al crear cliente';
+      },
+    });
+  }
 }
